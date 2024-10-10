efab14ab5a6b3148c02d20bc868fa52765736638
Brian Bravol <lumalabravo@gmail.com>
2024-10-10 18:32:46 +0300

    Update main.py

--- SQLite/lesson2/main.py
+++ SQLite/lesson2/main.py
@@ -7,11 +7,17 @@ cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS Population
              (City TEXT, Country TEXT, Population INT)''')
 
-file = open('population_data.csv')
-city_data = csv.reader(file)
-city_data.__next__()
-cursor.executemany('''INSERT INTO Population VALUES (?, ?, ?)''', city_data)
+# file = open('population_data.csv')
+# city_data = csv.reader(file)
+# city_data.__next__()
+# cursor.executemany('''INSERT INTO Population VALUES (?, ?, ?)''', city_data)
+#
 
+results =  cursor.execute('''SELECT * FROM Population''')
+# print(cursor.fetchall())
+
+for row in results:
+    print(row)
 
 connection.commit()
 connection.close()
