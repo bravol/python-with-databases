0b78bdab1e86fa5d6f72dfb1979999db9d7b86d0
Brian Bravol <lumalabravo@gmail.com>
2024-10-11 16:27:19 +0300

    Update main.py

--- MySQL/lesson1/main.py
+++ MySQL/lesson1/main.py
@@ -1,20 +1,35 @@
 import mysql.connector as mysql
 
-def connect(db_name, password):
 
+def connect(db_name):
     try:
-
         return mysql.connect(host='localhost',
-                           user='root',
-                           password=password,
-                           database=db_name)
+                             user='root',
+                             password='your_mysql_password',
+                             database=db_name)
     except Exception as e:
         print(e)
 
-db = connect('hr','CISSYbravol@75')
-cursor =db.cursor()
-cursor.execute('select * from employees')
-emp_records = cursor.fetchall()
-print(emp_records)
 
+def add_new_employee(cursor, name, position, task_descriptions):
+    new_emp = (name, position)
+    cursor.execute('''insert into employees(name, position) values (%s, %s)''', new_emp)
+    emp_id = cursor.lastrowid
+    tasks = []
+    for task in task_descriptions:
+        task_data = (emp_id, task)
+        tasks.append(task_data)
+    cursor.executemany('''insert into tasks(emp_id, description) values (%s, %s)''', tasks)
+
+
+db = connect('hr')
+cursor = db.cursor()
+tasks = ['Development', 'Documentation', 'Unit Testing']
+add_new_employee(cursor, 'John', 'Developer', tasks)
+
+# cursor.execute('select * from employees')
+# emp_record = cursor.fetchall()
+# print(emp_record)
+
+db.commit()
 db.close()
